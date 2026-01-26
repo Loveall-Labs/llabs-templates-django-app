ae3ab7ae8d56eabfb35dad41284cf0c9faf33684
Kenny Loveall <kenny@kennyloveall.com>
2026-01-26 03:22:35 -0600

    [fix] Fix duplicate 'app' label conflict in Django apps
    The library package's AppConfig now uses the package name instead of 'app'

--- {{cookiecutter.project_slug}}/{{cookiecutter.python_package_name}}/apps.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.python_package_name}}/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ExampleAppConfig(AppConfig):
+class {{ cookiecutter.python_package_name|replace('_', ' ')|title|replace(' ', '') }}Config(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "app"
+    name = "{{ cookiecutter.python_package_name }}"
