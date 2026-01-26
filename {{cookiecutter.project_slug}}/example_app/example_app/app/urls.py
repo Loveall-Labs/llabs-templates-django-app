4c633fd69e77d73b74714f69042cef65c5b7691a
Kenny Loveall <kenny@kennyloveall.com>
2026-01-26 02:32:48 -0600

    [fix] Fix pre-commit issues in generated project templates
    - Remove extra trailing blank lines from HTML templates
    - Fix import ordering in Python files (ruff isort)
    - Fix string continuation formatting in settings.py
    - Add blank line after docstring in urls.py
    - Remove unused import in package urls.py

--- {{cookiecutter.project_slug}}/example_app/example_app/app/urls.py
+++ {{cookiecutter.project_slug}}/example_app/example_app/app/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from app import views
 from django.urls import path
 from django.views.generic import TemplateView
-
-from app import views
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
--- {{cookiecutter.project_slug}}/{{cookiecutter.python_package_name}}/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.python_package_name}}/urls.py
@@ -17,8 +17,6 @@ Including another URLconf
 from django.urls import path
 from django.views.generic import TemplateView
 
-from app import views
-
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
 ]
